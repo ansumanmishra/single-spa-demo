7249574819550f419c95eaa93712df6760081f21
Ansuman Mishra <amishra@clickbuild.de>
2021-07-31 08:01:33 +0200

    Changing the application prefix from app to nav for navigation MFE

--- navigation/src/main.single-spa.ts
+++ navigation/src/main.single-spa.ts
@@ -16,7 +16,7 @@ const lifecycles = singleSpaAngular({
     singleSpaPropsSubject.next(singleSpaProps);
     return platformBrowserDynamic().bootstrapModule(AppModule);
   },
-  template: '<app-root />',
+  template: '<nav-root />',
   NgZone,
 });
 
