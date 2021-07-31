f6c8ced8a214dd7384b01f88f44ce6a0d81b8b8d
Ansuman Mishra <amishra@clickbuild.de>
2021-07-31 08:30:28 +0200

    Adding angular material to navigation MFE and setting up the routes

--- navigation/src/app/app.module.ts
+++ navigation/src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+
+const routes = [
+  {
+    path: '**',
+    component: EmptyRouteComponent,
+  },
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    MatToolbarModule,
+    MatIconModule,
+    RouterModule.forRoot(routes),
+  ],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
