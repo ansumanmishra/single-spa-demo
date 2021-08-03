31ff5e47a980dc1c2aac946852ce263906abb792
Ansuman Mishra <amishra@clickbuild.de>
2021-08-03 23:41:25 +0200

    Code cleanup

--- utility/src/demo-utility.ts
+++ utility/src/demo-utility.ts
@@ -1,6 +1,4 @@
 // Anything exported from this file is importable by other in-browser modules.
 import { BehaviorSubject } from "rxjs";
 
-export function publicApiFunction() {}
-
 export const keyword$ = new BehaviorSubject(undefined);
